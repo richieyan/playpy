f0c62f2b212d478da23ad9742fc9c8e7aa5530b1
Richie <richieyan@gmail.com>
2018-01-01 14:11:50 +0800

    update

--- src/dp/singleton.py
+++ src/dp/singleton.py
@@ -11,11 +11,14 @@ class Singleton(object):
 		except AttributeError:
 			self._instance = self._cls()
 			return self._instance
+	def test(self):
+		print('Singleton test method')
 	def __call__(self):
 		raise TypeError('Singletons must be accessed through Instance()')
 	def __instancecheck__(self,inst):
 		print '........'
 
+# _A == Singleon(A) --> A is Singleton Type
 @Singleton
 class A:
 	"""docstring for A"""
@@ -25,8 +28,10 @@ class A:
 		return id(self)
 
 if __name__ == '__main__':
+	print(A)
 	s1 = A.Instance()
 	s2 = A.Instance()
+	A.test()
 	print(s1,s1.display())
 	print(s2,s2.display())
 	print(s1 is s2)
